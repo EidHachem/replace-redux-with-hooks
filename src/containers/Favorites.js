fa907faa26fd47ec11b92a18eb9aa5fea82a06ad
eid <eidhachem1@gmail.com>
2024-09-21 21:30:20 +0300

    replace redux with useContext and prepare a custome hook

--- src/containers/Favorites.js
+++ src/containers/Favorites.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useContext } from "react"
 
-import FavoriteItem from '../components/Favorites/FavoriteItem';
-import './Products.css';
+import FavoriteItem from "../components/Favorites/FavoriteItem"
+import "./Products.css"
+import { ProductsContext } from "../context/products-ctx"
 
-const Favorites = props => {
-  const favoriteProducts = useSelector(state =>
-    state.shop.products.filter(p => p.isFavorite)
-  );
-  let content = <p className="placeholder">Got no favorites yet!</p>;
-  if (favoriteProducts.length > 0) {
+const Favorites = (props) => {
+  const favouriteProducts = useContext(ProductsContext).products.filter(
+    (prod) => prod.isFavorite
+  )
+  let content = <p className="placeholder">Got no favorites yet!</p>
+  if (favouriteProducts.length > 0) {
     content = (
       <ul className="products-list">
-        {favoriteProducts.map(prod => (
+        {favouriteProducts.map((prod) => (
           <FavoriteItem
             key={prod.id}
             id={prod.id}
@@ -21,9 +21,9 @@ const Favorites = props => {
           />
         ))}
       </ul>
-    );
+    )
   }
-  return content;
-};
+  return content
+}
 
-export default Favorites;
+export default Favorites
